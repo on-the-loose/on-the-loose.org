79fe331aaf8e4a9b1477e54a183ea3b6f90bed35
simonpfish <simonpfish@gmail.com>
2018-11-17 11:19:07 -0800

    add signout

--- client/components/UserAvatar.tsx
+++ client/components/UserAvatar.tsx
@@ -1,19 +1,50 @@
 import * as React from 'react'
-import { Avatar } from 'antd'
+import { Popover, Button } from 'antd'
 
-import 'antd/lib/avatar/style/css'
+import 'antd/lib/popover/style/css'
+import 'antd/lib/button/style/css'
+
 import firebase from 'firebase'
-
-// TODO add signout
 
 export interface Props {
   user: firebase.User
 }
 
-const AccountAvatar: React.SFC<Props> = props => {
-  const user = firebase.auth().currentUser
-
-  return <Avatar size="large">{user.email[0].toUpperCase()}</Avatar>
+export interface State {
+  show_popover: boolean
 }
 
-export default AccountAvatar
+export default class AccountAvatar extends React.Component<Props, State> {
+  state = { show_popover: false }
+
+  handleSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
+        // Sign-out successful.
+      })
+      .catch(function(error) {
+        // An error happened.
+      })
+  }
+
+  public render() {
+    return (
+      <Popover
+        placement="bottomLeft"
+        content={<a onClick={this.handleSignOut}>Sign out</a>}
+        title={this.props.user.email}
+        trigger="click"
+        visible={this.state.show_popover}
+        onVisibleChange={show_popover => {
+          this.setState({ show_popover })
+        }}
+      >
+        <Button shape="circle" icon="user" size="large" />
+      </Popover>
+    )
+
+    // <Avatar size="large">{this.props.user.email[0].toUpperCase()}</Avatar>
+  }
+}
