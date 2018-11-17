d0d945ac09596bac6458c955a7a2321a5db637ec
simonpfish <simonpfish@gmail.com>
2018-11-16 17:04:32 -0800

    connect to firebase

---
+++ client/firebase.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebase_config = {
+  apiKey: 'AIzaSyCbdmTfvPp334ZhII9t2vxDACLUDJ_pfm0',
+  authDomain: 'on-the-loose.firebaseapp.com',
+  databaseURL: 'https://on-the-loose.firebaseio.com',
+  projectId: 'on-the-loose',
+  storageBucket: 'on-the-loose.appspot.com',
+  messagingSenderId: '816227148735'
+}
+
+firebase.initializeApp(firebase_config)
+
+const db = firebase.firestore()
+
+db.settings({
+  timestampsInSnapshots: true
+})
+
+export default firebase
