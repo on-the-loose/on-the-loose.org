6be6a9a8fe7fb2dfbab7fcad9821df3323b5451a
Simon Posada Fishman <spf@fb.com>
2022-08-02 14:51:16 -0700

    clarify api key purpose

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,6 +5,7 @@ import 'firebase/functions'
 import firebase from 'firebase/app'
 
 const firebase_config = {
+  // It's okay for this key to be public (https://firebase.google.com/docs/projects/api-keys#api-keys-for-firebase-are-different)
   apiKey: 'AIzaSyCbdmTfvPp334ZhII9t2vxDACLUDJ_pfm0',
   authDomain: 'on-the-loose.firebaseapp.com',
   databaseURL: 'https://on-the-loose.firebaseio.com',
