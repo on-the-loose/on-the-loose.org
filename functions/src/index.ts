83901da5f03ba6b9b846d28a651ea07a54930922
Simón Posada Fishman <spf@fb.com>
2022-08-03 23:54:51 -0700

    update functions dependencies

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,28 +6,30 @@ import * as _ from 'lodash'
 import { orderBy } from 'natural-orderby'
 import 'firebase/auth'
 import 'firebase/firestore'
+import { getAuth, sendSignInLinkToEmail } from 'firebase/auth'
 
 // If you change this regex make sure to also change the one in the front-end (ProfileForm.tsx)
-export const EMAIL_REGEX = /.+(@pomona\.edu|@mymail.pomona\.edu|@cmc\.edu|@hmc\.edu|@g\.hmc\.edu|@scrippscollege\.edu|@pitzer\.edu|@students\.pitzer\.edu|@cgu\.edu|@kgi\.edu)/
+export const EMAIL_REGEX =
+  /.+(@pomona\.edu|@mymail.pomona\.edu|@cmc\.edu|@hmc\.edu|@g\.hmc\.edu|@scrippscollege\.edu|@pitzer\.edu|@students\.pitzer\.edu|@cgu\.edu|@kgi\.edu)/
 
 // Emails that get notifications when trips are created or modified
 const EMAIL_LIST = [
   'otlstaff@gmail.com',
   'oec@pomona.edu',
   'chris.Weyant@pomona.edu',
-  'martin.crawford@pomona.edu'
+  'martin.crawford@pomona.edu',
 ]
 
 admin.initializeApp()
 const db = admin.firestore()
 
-firebase.initializeApp({
+const app = firebase.initializeApp({
   apiKey: 'AIzaSyCbdmTfvPp334ZhII9t2vxDACLUDJ_pfm0',
   authDomain: 'on-the-loose.firebaseapp.com',
   databaseURL: 'https://on-the-loose.firebaseio.com',
   projectId: 'on-the-loose',
   storageBucket: 'on-the-loose.appspot.com',
-  messagingSenderId: '816227148735'
+  messagingSenderId: '816227148735',
 })
 
 const gmailEmail = functions.config().gmail.email
@@ -36,8 +38,8 @@ const mailTransport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: gmailEmail,
-    pass: gmailPassword
-  }
+    pass: gmailPassword,
+  },
 })
 
 export const checkAccountExists = functions.https.onCall((data, context) => {
@@ -45,13 +47,13 @@ export const checkAccountExists = functions.https.onCall((data, context) => {
     .collection('users')
     .doc(data.email.toLowerCase())
     .get()
-    .then(doc => doc.exists)
+    .then((doc) => doc.exists)
 
   return admin
     .auth()
     .getUserByEmail(data.email.toLowerCase())
-    .then(_ => accountDataCheck)
-    .catch(_ => accountDataCheck)
+    .then((_) => accountDataCheck)
+    .catch((_) => accountDataCheck)
 })
 
 export const createAccount = functions.https.onCall(async (data, context) => {
@@ -66,13 +68,13 @@ export const createAccount = functions.https.onCall(async (data, context) => {
   await admin
     .auth()
     .getUserByEmail(email)
-    .then(_ => (accountExists = true))
-    .catch(_ => (accountExists = false))
+    .then((_) => (accountExists = true))
+    .catch((_) => (accountExists = false))
 
   return db
     .doc(`users/${email}`)
     .get()
-    .then(value => {
+    .then((value) => {
       if (value.exists && accountExists) throw new Error('Account already exists')
       console.log(`Creating account for: ${email}`)
       return Promise.all([
@@ -80,13 +82,11 @@ export const createAccount = functions.https.onCall(async (data, context) => {
           .doc(`users/${email}`)
           .set(data.account)
           .then(() => console.log(`Successfully created account for: ${email}`)),
-        firebase
-          .auth()
-          .sendSignInLinkToEmail(email, {
-            url: data.url,
-            handleCodeInApp: true
-          })
-          .then(() => console.log(`Successfully sent first sign in link to: ${email}`))
+
+        sendSignInLinkToEmail(getAuth(app), email, {
+          url: data.url,
+          handleCodeInApp: true,
+        }).then(() => console.log(`Successfully sent first sign in link to: ${email}`)),
       ])
     })
 })
@@ -103,13 +103,13 @@ export const onTripCreation = functions.firestore
   })
 
 async function sendTripDataEmail(id, toEmails, subject) {
-  const mailOptions = toEmails.map(email => ({
+  const mailOptions = toEmails.map((email) => ({
     from: `On The Loose <${gmailEmail}>`,
     to: email,
     subject,
-    text: `Access the trip here: https://on-the-loose.org/trips/${id}`
+    text: `Access the trip here: https://on-the-loose.org/trips/${id}`,
   }))
 
-  await Promise.all(mailOptions.map(options => mailTransport.sendMail(options)))
+  await Promise.all(mailOptions.map((options) => mailTransport.sendMail(options)))
   console.log('Trip creation email notification sent')
 }
