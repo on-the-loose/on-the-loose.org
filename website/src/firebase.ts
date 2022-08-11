cb656042933937b5fac711e479f1ec3b186a3234
Simón Posada Fishman <spf@fb.com>
2022-08-11 10:17:18 -0700

    connect functions to emulator

--- website/src/firebase.ts
+++ website/src/firebase.ts
@@ -5,7 +5,7 @@ import 'firebase/functions'
 import { initializeApp } from 'firebase/app'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getFunctions } from 'firebase/functions'
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions'
 
 const firebaseConfig = {
   // It's okay for this key to be public (https://firebase.google.com/docs/projects/api-keys#api-keys-for-firebase-are-different)
@@ -23,5 +23,7 @@ export const db = getFirestore(app)
 export const functions = getFunctions(app)
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  console.log('Connecting to Firebase Emulators')
+  connectFunctionsEmulator(functions, 'localhost', 4001)
   connectAuthEmulator(auth, 'http://localhost:9099')
 }
